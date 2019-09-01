911f8459a7c625ae03902873136b49f0a09b0ace
m-yoshiro <massugu.massugu@gmail.com>
2019-09-01 09:57:44 +0900

    setup handlebars

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,5 @@
 import { storiesOf } from '@storybook/html';
+import Button from '../src/button/button.hbs'
 
 storiesOf('Button', module)
   .add('with text', () => '<button class="">Hello World</button>')
@@ -6,4 +7,7 @@ storiesOf('Button', module)
     const button = document.createElement('button');
     button.innerText = '😀 😎 👍 💯';
     return button;
-  });
+  })
+  .add('With handlebars', () => {
+    return Button({text: 'button'});
+  })
